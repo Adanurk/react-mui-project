b485dafcaa3a9a861c97b3bde73afb98748068d9
Adanurk <gumussoykbr@gmail.com>
2022-09-13 23:25:56 +0200

    sidebar in progress

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,46 @@
 import React from 'react';
 import { Box} from "@mui/system";
+import { ListItemIcon, List, ListItemText, ListItem, ListItemButton } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
 
 const Sidebar = () => {
   return (
-    <Box bgcolor="skyblue" flex={1} p={2} sx={{display:{xs:"none", sm:"block"}}}>Sidebar</Box>
+    <Box flex={1} p={2} sx={{display:{xs:"none", sm:"block"}}}>
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#home">
+              <ListItemIcon>
+                <HomeIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#pages">
+              <ListItemIcon>
+                <HomeIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#groups">
+              <ListItemIcon>
+                <HomeIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#marketplace">
+              <ListItemIcon>
+                <HomeIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+    </Box>
   )
 }
 
